bfdd8b7c7280af97073f0fa34586ec1ddce359ca
Matt Carter <matt@showdeo.com>
2017-04-25 11:51:28 -0700

    adds default values to email preferences in user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  def create
+    User.create(user_params)
+  end
+
+  def edit
+    @user = User.find(params[:email])
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params
+    params.require(:name, :email)
+  end
 end
