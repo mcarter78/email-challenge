236f9c8dce6eb244a2b4fbdce7a955969294290f
Matt Carter <matt@showdeo.com>
2017-04-25 20:51:43 -0700

    builds Token.consume method and fixes validations in users#create & users#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,24 +15,12 @@ class UsersController < BaseController
 
   def create
     # VALIDATE THAT THE EMAIL SUBMITTED DOES NOT ALREADY EXIST IN DB
-    # User.find will throw an error if it does not find the requested record,
-    # so I will find all users, then iterate through the returned data to
-    # check for any existing email address matches.
-    # This would not be a good solution in a large database, and is my
-    # first priortity to refactor once the application is fully functional.
-    users = User.all
-    matches = 0
-    for user in users
-      if user[:email] == params[:email]
-        matches = matches + 1
-      end
-    end
-
-    if matches == 0
+    users = User.find_by(email: params[:email])
+    if users == nil
       new_user = User.create(user_params)
       respond_with new_user, json: new_user
     else
-      error_msg = ['error']
+      error_msg = ['Error: Email already exists!']
       respond_with error_msg, json: error_msg
     end
   end
@@ -44,27 +32,20 @@ class UsersController < BaseController
 
   def update
     # VALIDATE THAT THE EMAIL SUBMITTED DOES NOT ALREADY EXIST IN DB
-    # User.find will throw an error if it does not find the requested record,
-    # so I will find all users, then iterate through the returned data to
-    # check for any existing email address matches.
-    # This would not be a good solution in a large database, and is my
-    # first priortity to refactor once the application is fully functional.
-    users = User.all
-    matches = 0
-    for user in users
-      if user[:email] == params[:email]
-        matches = matches + 1
-      end
-    end
-
-    if matches == 0
+    users = User.find_by(email: params[:email])
+    if users == nil
       user = User.find(params[:id])
       # TODO: validate that token in params matches a nonce tied to current user email
-
-      user.update_attributes(user_params)
-      respond_with user, json: user
+      consumed = Token.consume params[:token]
+      if consumed != nil
+        user.update_attributes(user_params)
+        respond_with user, json: user
+      else
+        error_msg = ['Error: Incorrect Token!']
+        respond_with error_msg, json: error_msg
+      end
     else
-      error_msg = ['error']
+      error_msg = ['Error: Email already exists!']
       respond_with error_msg, json: error_msg
     end
 
--- app/models/token.rb
+++ app/models/token.rb
@@ -1,13 +1,34 @@
 class Token < ActiveRecord::Base
   def self.generate (user)
-    puts user
+    # Convert the given id integer to a string
     id = user[:id].to_s
+    # Concatenate the id and a random number, joined with an underscore
     nonce = id + "_" + SecureRandom.base64
-    return nonce
+    # Build a new token object
+    token = {
+      nonce: nonce,
+      user_id: user[:id]
+    }
+    # Use the token object to create a new token in the DB
+    new_token = Token.create(token)
+    # return the new token
+    return new_token
   end
 
-  def self.consume (token)
-    puts token
-    return token
+  def self.consume (nonce)
+    # Try to find a matching token object by the nonce given
+    token = Token.find_by(nonce: nonce)
+    # If token exists
+    if token != nil
+      # Find the associated user by their id in the token object
+      user = User.find_by(id: token[:user_id])
+      # Destroy the token
+      token.destroy
+      # Return the user object
+      return user
+    else
+      # If the token does not exist...
+      return nil
+    end
   end
 end
